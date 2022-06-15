6bdac3d8d21ecf56acd7b012324598ecbd14f7ca
Neto Evtek <jsn.ads@gmail.com>
2022-06-15 10:01:49 -0300

    SGES V1 modulo mvc php sem framework

---
+++ SGES/src/Config.php
@@ -0,0 +1,15 @@
+<?php
+namespace src;
+
+class Config {
+    const BASE_DIR = '/PROJETOS/SGES/public/';
+
+    const DB_DRIVER = 'mysql';
+    const DB_HOST = '127.0.0.1';
+    const DB_DATABASE = 'sges';
+    CONST DB_USER = 'adm@adm';
+    const DB_PASS = '229536';
+
+    const ERROR_CONTROLLER = 'ErrorController';
+    const DEFAULT_ACTION = 'index';
+}
