e169f49a423d338d99936b6fdf35e3ab4662c02c
Neto Evtek <jsn.ads@gmail.com>
2022-07-01 17:30:41 -0300

    SGES V1 banco atualizado

--- SGES/00-documentacao/sges.sql
+++ SGES/00-documentacao/sges.sql
@@ -54,9 +54,9 @@ CREATE TABLE IF NOT EXISTS `movimentacoes` (
 -- Copiando estrutura para tabela sges.pessoas
 CREATE TABLE IF NOT EXISTS `pessoas` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nome` varchar(200) NOT NULL,
-  `data_nasc` date NOT NULL,
-  `telefone` varchar(15) NOT NULL,
+  `nome` varchar(200) DEFAULT NULL,
+  `data_nasc` date DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
   `cep` varchar(10) DEFAULT NULL,
   `rua` varchar(200) DEFAULT NULL,
   `qd` varchar(5) DEFAULT NULL,
@@ -65,14 +65,16 @@ CREATE TABLE IF NOT EXISTS `pessoas` (
   `bairro` varchar(200) DEFAULT NULL,
   `cidade` varchar(200) DEFAULT NULL,
   `estado` varchar(2) DEFAULT NULL,
-  `id_user` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_pessoas_users` (`id_user`),
   CONSTRAINT `FK_pessoas_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- Copiando dados para a tabela sges.pessoas: ~0 rows (aproximadamente)
 /*!40000 ALTER TABLE `pessoas` DISABLE KEYS */;
+INSERT INTO `pessoas` (`id`, `nome`, `data_nasc`, `telefone`, `cep`, `rua`, `qd`, `lt`, `num`, `bairro`, `cidade`, `estado`, `id_user`) VALUES
+	(11, 'Jose Alves de Souza Neto', '1989-11-20', '(62)9 9351-8323', '74.369-660', 'mmm10', '36', '38', '2', 'tres marias III', 'goiania', 'GO', 1);
 /*!40000 ALTER TABLE `pessoas` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela sges.users
@@ -82,13 +84,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(200) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `token` varchar(200) NOT NULL,
+  `navBar` varchar(200) DEFAULT NULL,
+  `card` varchar(200) DEFAULT NULL,
   `created_at` date DEFAULT NULL,
   `update_at` date DEFAULT NULL,
+  `Coluna 10` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela sges.users: ~0 rows (aproximadamente)
+-- Copiando dados para a tabela sges.users: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `email`, `password`, `nome`, `token`, `navBar`, `card`, `created_at`, `update_at`, `Coluna 10`) VALUES
+	(1, 'jsn@gmail.com', '$2y$10$t5ZuYh9R9pQGrk4DC/snO.EbAOWnRzeie8JBjUP.p9ogt..TVx3cW', 'Jose Neto', 'a31b0c5f8dc52194b916a9cbddb128bf', 'dark', '', NULL, NULL, NULL),
+	(2, 'cris@gmail.com', '$2y$10$IKc6Pdsbj56TujZx8NxYPu15mPNAXhivE23B2UlIpraWZdgM.xZUq', 'Cristina ', 'ba6a4588bf0c3ed3e2ac85f0a260bc76', 'blue', NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
